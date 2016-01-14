460474d0fc4b38c8ed1eb44dc861da0dc9b8edd0
neolinks <ernarzhumashev@gmail.com>
2016-01-15 00:33:53 +0600

    ecotaxi
    at_home

--- resources/views/src/personal.js
+++ resources/views/src/personal.js
@@ -1,25 +1,35 @@
 import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
-import {bootstrapDropdownHover} from 'bootstrap/js/jquery.bootstrap-dropdown-hover.min';
 import 'fetch';
 import 'bootstrap';
-import $ from 'bootstrap';
 @inject(HttpClient)
 export class Personal{
-    dropdown(){
-        $('ul.dropdown-menu').stop(true, true).delay(100).fadeIn();
-        $('ul.dropdown-menu').mouseover(function(){
-            $('ul.dropdown-menu').css("display","block");
-        })
-    }
-    close(){
-        $('ul.dropdown-menu').stop(true, true).delay(100).fadeOut();
-    }
 
-    source;
-    dest;
-    sourceTime;
-    phone;
-    passanger;
-    comment;
+    orderParams =[
+         {name : 'source-address', displayName : 'Адрес подачи', value : this.sourceAddress},
+         {name : 'dest-adress', displayName : 'Адрес назначения', value : this.destAddress},
+         {name : 'source-time', displayName : 'Время', value : this.sourceTime},
+         {name : 'phone', displayName : 'Телефон', value : this.phone},
+         {name : 'passenger', displayName : 'Пассажир', value : this.passenger},
+         {name : 'comment', displayName : 'Комментарий', value : this.comment}
+    ];
+    //params = {
+    //    sourceAddress: {name: 'source-address', displayName: 'Адрес подачи', value: ''},
+    //    destAddress: {name: 'dest-address', displayName: 'Адрес назначения', value: ''},
+    //    sourceTime: {name: 'source-time', displayName: 'Время', value: ''},
+    //    phone: {name: 'phone', displayName: 'Телефон', value: ''},
+    //    passanger: {name: 'passenger', displayName: 'Пассажир', value: ''},
+    //    comment: {name: 'comment', displayName: 'Комментарий', value: ''}
+    //};
+    constructor(){
+        this.sourceAddress ='';
+        this.destAddress ='';
+        this.sourceTime ='';
+        this.phone='';
+        this.passenger='';
+        this.comment = 'test';
+    }
+    clear(){
+        console.log(this.sourceAddress);
+    }
 }
